00a6b7a6cd70cf2cad16c1855907ed92dd45136a
Sarah Cardoso <sarahcardososouza@gmail.com>
2025-02-11 01:49:25 +0000

    Changes of controllers to views

--- app/controllers/mentoras_controller.rb
+++ app/controllers/mentoras_controller.rb
@@ -1,4 +1,21 @@
 class MentorasController < ApplicationController
   def index
+    @mentoras = Mentora.all
+  end
+
+  def new
+    @mentora = Mentora.new
+  end
+
+  def create
+    @mentora = Mentora.new(name: params[:mentora][:name], email: params[:mentora][:email], abilities: params[:mentora][:abilities])
+
+    if @mentora.save
+      redirect_to mentora_path(@mentora)
+    end
+  end
+
+  def show
+    @mentora = Mentora.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
-  get "mentoras/index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "mentoras#index"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  resources :mentoras
+  # get "mentoras/index"
+  # get "mentoras/new", to: "mentoras#new"
+  # post "mentoras", to: "mentoras#create"
+  # get "mentoras/:id", to: "mentoras#show", as: "mentora"
+
+
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
